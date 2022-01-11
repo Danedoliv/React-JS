bf075f0670cf4792bdf6bd9331b661f41d16beac
Daniel Olivier <danielolivier8@hotmail.com>
2022-01-10 22:23:20 -0300

    desafio clase 12

--- src/Componentes/Item.jsx
+++ src/Componentes/Item.jsx
@@ -12,11 +12,12 @@ export default function Item({item}) {
                 <h5 className="card-title text-uppercase">{item.name}</h5>
                 <h5 className="card-subtitle mb-2 text-muted text-uppercase">{item.category}</h5>
                 <p className="card-text fs-3">${item.price} </p>
-               
+                <NavLink to="/item/id:" className="btn btn-primary w-100 my-2 text-uppercase">Show detail</NavLink>
+                <ItemCount stock={item.stock} initial={1} item={item}/>
             </div>
-            <NavLink to="/item/id:" className="btn btn-primary w-100 my-2 text-uppercase">Show detail</NavLink>
+            
          </div>
-         <ItemCount stock={item.stock} initial={1} item={item}/>
+         
      </div>  
     )
 }
